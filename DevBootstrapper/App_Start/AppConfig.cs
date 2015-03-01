fcfc13e36264855e388f010202c2ae0a4bf231b1
Alim Ul Karim <devorg.bd@gmail.com>
2015-03-01 15:42:01 +0600

    fixing structure

--- DevBootstrapper/App_Start/AppConfig.cs
+++ DevBootstrapper/App_Start/AppConfig.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Linq;
-using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
-using DevMVCComponent;
-using DevMVCComponent.Mailers;
 using DevBootstrapper.Models.Context;
 using DevBootstrapper.Models.POCO.IdentityCustomization;
 using DevBootstrapper.Modules.Cache;
@@ -13,64 +10,83 @@ using DevBootstrapper.Modules.Mail;
 using DevBootstrapper.Modules.Session;
 using DevBootstrapper.Modules.TimeZone;
 using DevBootstrapper.Modules.UserError;
+using System.Configuration;
 
 namespace DevBootstrapper {
     /// <summary>
-    ///     Application Configurations, also contains the list of roles.
+    /// Application Configurations, also contains the list of roles.
     /// </summary>
     public static class AppConfig {
-        private static CoreSetting setting;
-        private static bool initalized;
-        private static int _truncateLength = 30;
 
-        public static int ValidationMaxNumber {
-            get { return 10; }
-        }
+        #region Public declares
 
-        public static int TruncateLength {
-            get { return _truncateLength; }
-            set { _truncateLength = value; }
-        }
+        public static CookieProcessor Cookies = new CookieProcessor();
+        public static CacheProcessor Caches = new CacheProcessor();
+        public static ErrorCollector ErrorCollection = new ErrorCollector();
+        public static readonly string[] Roles = new string[] {
+            "Admin",
+            "Moderator",
+            "Default",
+            "Guest"
+        };
 
-        /// <summary>
-        ///     Get few common classes from Developers Organism Component.
-        /// </summary>
-        public static CoreSetting Setting {
-            get {
-                if (setting == null) {
-                    using (var db = new DevIdentityDbContext()) {
-                        setting = db.CoreSettings.FirstOrDefault();
-                    }
-                }
-                return setting;
-            }
-        }
-
+        #endregion
         public static ErrorCollector GetNewErrorCollector() {
             return new ErrorCollector();
         }
+        private static CoreSetting _setting = null;
+        private static bool _initalized = false;
+        private static int _truncateLength = 30;
+
+        public static int ValidationMaxNumber { get { return 10; } }
+
+        public static int TruncateLength {
+            get {
+                return _truncateLength;
+            }
+            set {
+                _truncateLength = value;
+            }
+        }
+
 
         private static void InitalizeDevelopersOrganismComponent(bool force = false) {
-            if (!initalized || force) {
-                Config.ApplicationName = AppVar.Name;
-                Config.AdminEmail = Setting.AdminEmail;
-                Config.DeveloperEmail = Setting.DeveloperEmail;
-                Config.Assembly = Assembly.GetExecutingAssembly();
+            if (!_initalized || force) {
+                DevMVCComponent.Config.ApplicationName = AppVar.Name;
+                DevMVCComponent.Config.AdminEmail = Setting.AdminEmail;
+                DevMVCComponent.Config.DeveloperEmail = Setting.DeveloperEmail;
+                DevMVCComponent.Config.Assembly = System.Reflection.Assembly.GetExecutingAssembly();
                 Zone.LoadTimeZonesIntoMemory();
-                initalized = true;
+                _initalized = true;
             }
         }
 
         /// <summary>
-        ///     Settings will not be null. Default values will be pushed.
+        /// Get few common classes from Developers Organism Component.
+        /// </summary>
+
+
+        public static CoreSetting Setting {
+            get {
+                if (_setting == null) {
+                    using (DevIdentityDbContext db = new DevIdentityDbContext()) {
+                        _setting = db.CoreSettings.FirstOrDefault();
+                    }
+                }
+                return _setting;
+            }
+        }
+
+        /// <summary>
+        /// Settings will not be null. Default values will be pushed.
         /// </summary>
         /// <returns></returns>
         public static bool CreateDefaultCoreSetting() {
             var s = Setting;
             if (s == null) {
                 //no setting exist , need to create a default setting.
-                using (var db = new DevIdentityDbContext()) {
-                    setting = new CoreSetting {
+                using (DevIdentityDbContext db = new DevIdentityDbContext()) {
+                    _setting = new CoreSetting() {
                         // Set the id to be auto in db.
                         CoreSettingID = 1,
                         ApplicationName = "Developers Organism Component",
@@ -110,42 +126,44 @@ namespace DevBootstrapper {
                         IsSMTPSSL = true,
                         IsFirstUserFound = false
                     };
-                    db.CoreSettings.Add(setting);
-                    var i = db.SaveChanges();
+                    db.CoreSettings.Add(_setting);
+                    int i = db.SaveChanges();
                     if (i >= 0) {
                         return true;
+                    } else {
+                        return false;
                     }
-                    return false;
                 }
             }
             return false;
         }
 
         public static void RefreshSetting() {
+
             using (var db = new DevIdentityDbContext()) {
                 CreateDefaultCoreSetting();
 
-                setting = db.CoreSettings.FirstOrDefault();
-                if (setting == null) {
+                _setting = db.CoreSettings.FirstOrDefault();
+                if (_setting == null) {
                     throw new Exception("Couldn't create or get the core settings. Please check the creation.");
                 }
                 InitalizeDevelopersOrganismComponent(true);
                 AppVar.IsInTestEnvironment = Setting.IsInTestingEnvironment;
 
-                AppVar.Name = Setting.ApplicationName;
-                AppVar.Subtitle = Setting.ApplicationSubtitle;
+                AppVar.Name = Setting.ApplicationName.ToString();
+                AppVar.Subtitle = Setting.ApplicationSubtitle.ToString();
                 AppVar.Setting = Setting;
                 AppVar.SetCommonMetaDescriptionToEmpty();
                 //Configure this with add a sender email.
-                Starter.Mailer = new CustomMailConfig(Setting.SenderEmail, Setting.SenderEmailPassword, Setting.SmtpHost,
-                    Setting.SmtpMailPort, Setting.IsSMTPSSL);
+                DevMVCComponent.Starter.Mailer = new DevMVCComponent.Mailers.CustomMailConfig(Setting.SenderEmail, Setting.SenderEmailPassword, Setting.SmtpHost, Setting.SmtpMailPort, Setting.IsSMTPSSL);
                 //if false then no email on error.
-                Config.IsNotifyDeveloper = Setting.NotifyDeveloperOnError;
+                DevMVCComponent.Config.IsNotifyDeveloper = Setting.NotifyDeveloperOnError;
+
             }
         }
 
         /// <summary>
-        ///     Get error and set it to null.
+        /// Get error and set it to null.
         /// </summary>
         /// <returns></returns>
         public static ErrorCollector GetGlobalError() {
@@ -153,83 +171,71 @@ namespace DevBootstrapper {
                 var error = (ErrorCollector)HttpContext.Current.Session[SessionNames.Error];
                 HttpContext.Current.Session[SessionNames.Error] = null;
                 return error;
+            } else {
+                return null;
             }
-            return null;
         }
 
         /// <summary>
-        ///     Set Global Error
+        /// Set Global Error
         /// </summary>
         /// <param name="error"></param>
         public static void SetGlobalError(ErrorCollector error) {
             HttpContext.Current.Session[SessionNames.Error] = error;
         }
 
-        #region Public declares
-
-        public static CookieProcessor Cookies = new CookieProcessor();
-        public static CacheProcessor Caches = new CacheProcessor();
-        public static ErrorCollector ErrorCollection = new ErrorCollector();
-
-        public static readonly string[] Roles = {
-            "Admin",
-            "Moderator",
-            "Default",
-            "Guest"
-        };
-
-        #endregion
     }
-
     /// <summary>
-    ///     Application Global Variables
+    /// Application Global Variables
     /// </summary>
     public struct AppVar {
+
         #region Enums
 
         #endregion
 
         #region Constants
 
+
+
+
         #endregion
 
         #region Connection Strings and Constants
+        //public const string DefaultConnection = @"Data Source=(LocalDb)\v11.0;AttachDbFilename=|DataDirectory|\WereViewApp-Accounts.mdf;Initial Catalog=WereViewApp-Accounts;Integrated Security=True";
 
-        public const string DefaultConnection =
-            @"Data Source=(LocalDb)\v11.0;AttachDbFilename=|DataDirectory|\asp.net-Maganize.mdf;Initial Catalog=asp.net-Maganize;Integrated Security=True";
+        private static readonly string DefaultConnection =
+             ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
 
         public enum ConnectionStringType {
             DefaultConnection,
             Secondary
         };
 
+
         #endregion
 
         #region Propertise
-
-        private static string _productNameMeta;
-
+        static string _productNameMeta;
         /// <summary>
-        ///     Application Name
+        /// Application Name
         /// </summary>
         public static string Name;
-
         /// <summary>
-        ///     Application Subtitle
+        /// Application Subtitle
         /// </summary>
         public static string Subtitle;
-
         /// <summary>
-        ///     Is application in testing environment or not?
+        /// Is application in testing environment or not?
         /// </summary>
         public static bool IsInTestEnvironment;
 
 
-        public static CoreSetting Setting;
 
+        public static CoreSetting Setting;
         /// <summary>
-        ///     Get the application URL based on the application environment.
-        ///     Without slash.
+        /// Get the application URL based on the application environment.
+        /// Without slash.
         /// </summary>
         public static string Url {
             get {
@@ -241,10 +247,11 @@ namespace DevBootstrapper {
         }
 
         public static MailSender Mailer = new MailSender();
-
         #endregion
 
         #region Functions
+
+
 
         public static string GetConnectionString(ConnectionStringType type) {
             switch (type) {
@@ -257,13 +264,12 @@ namespace DevBootstrapper {
             }
             return null;
         }
-
-        private static string getCommonMetadescription() {
-            var finalMeta = "";
+        static string GetCommonMetadescription() {
+            string finalMeta = "";
             if (_productNameMeta == null) {
-                var nameList = Name.Split(' ').ToList();
-                nameList.Add(Name);
-                nameList.Add(Subtitle);
+                var nameList = AppVar.Name.Split(' ').ToList();
+                nameList.Add(AppVar.Name);
+                nameList.Add(AppVar.Subtitle);
                 foreach (var item in nameList) {
                     if (finalMeta.Equals("")) {
                         finalMeta += ",";
@@ -274,41 +280,38 @@ namespace DevBootstrapper {
             }
             return _productNameMeta;
         }
-
         internal static void SetCommonMetaDescriptionToEmpty() {
             _productNameMeta = null;
         }
 
         public static ActionResult GetFriendlyError(string title, string message) {
-            var dictionary = new ViewDataDictionary {
-                {"Title", title},
-                {"ErrorMessage", message}
+            var dictionary = new ViewDataDictionary(){
+              {"Title",title},
+              {"ErrorMessage",message}
             };
-            return new ViewResult {
+            return new ViewResult() {
                 ViewName = "_FriendlyError",
                 ViewData = dictionary
             };
         }
 
         public static ActionResult GetAuthenticationError(string title, string message) {
-            var dictionary = new ViewDataDictionary {
-                {"Title", title},
-                {"ErrorMessage", message}
+            var dictionary = new ViewDataDictionary(){
+              {"Title", title},
+              {"ErrorMessage",message}
             };
-            return new ViewResult {
+            return new ViewResult() {
                 ViewName = "_AuthenticationError",
                 ViewData = dictionary
             };
         }
 
-        public static void GetTitlePageMeta(dynamic viewBag, string title, string msg = "", string meta = null,
-            string keywords = null) {
+        public static void GetTitlePageMeta(dynamic viewBag, string title, string msg = "", string meta = null, string keywords = null) {
             viewBag.Title = title;
             viewBag.Message = msg;
-            viewBag.Meta = meta + "," + getCommonMetadescription();
-            viewBag.Keywords = keywords + "," + getCommonMetadescription();
+            viewBag.Meta = meta + "," + GetCommonMetadescription();
+            viewBag.Keywords = keywords + "," + GetCommonMetadescription();
         }
-
         public static void SetSavedStatus(dynamic viewBag, string msg = null) {
             if (msg == null) {
                 msg = "Your previous transaction is successfully saved.";
@@ -322,7 +325,7 @@ namespace DevBootstrapper {
             }
             viewBag.ErrorSave = msg;
         }
-
         #endregion
+
     }
 }
