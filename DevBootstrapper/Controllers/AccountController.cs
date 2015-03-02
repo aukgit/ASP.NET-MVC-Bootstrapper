a86fc6d40bd0808baa1235ac0162208f9b4f06bf
Alim Ul Karim <devorg.bd@gmail.com>
2015-03-02 11:49:07 +0600

    register view fixed for dropdown country
    register view fixed for dropdown country

--- DevBootstrapper/Controllers/AccountController.cs
+++ DevBootstrapper/Controllers/AccountController.cs
@@ -2,7 +2,6 @@
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using DevBootstrapper.Filter;
 using DevBootstrapper.Models.Context;
 using DevBootstrapper.Models.POCO.Identity;
 using DevBootstrapper.Models.ViewModels;
