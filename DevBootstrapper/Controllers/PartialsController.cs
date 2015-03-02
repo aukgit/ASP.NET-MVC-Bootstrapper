d47e5069b8fb326ff6f63ec6d12ec5b295976fcd
Alim Ul Karim <devorg.bd@gmail.com>
2015-03-03 03:15:03 +0600

    t4 controller completed

---
+++ DevBootstrapper/Controllers/PartialsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DevBootstrapper.Controllers;
+using DevBootstrapper.Models.Context;
+using DevBootstrapper.Models.EntityModel.POCO;
+using DevTrends.MvcDonutCaching;
+
+namespace DevBootstrapper.Controllers
+{
+    [DonutOutputCache(CacheProfile = "YearNoParam")]
+    public class PartialsController : GenericController<MagazineEntities> {
+
+    }
+	
+}
