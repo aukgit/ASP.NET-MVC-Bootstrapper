4af20fdce12847fca1b3207cffe4233aed2e5bbf
Alim Ul Karim <devorg.bd@gmail.com>
2015-03-07 00:17:12 +0600

    t4 edited

--- DevBootstrapper/Controllers/PartialsController.cs
+++ DevBootstrapper/Controllers/PartialsController.cs
@@ -34,6 +34,22 @@ namespace DevBootstrapper.Controllers
         }
 
         #endregion
+
+        #region ProductOrdersController : DropDowns to paste into the partial
+
+        // [DonutOutputCache(CacheProfile = "YearNoParam")]
+        public JsonResult GetCustomerID() {
+            var data = db.Customers.Select(n => new { id = n.CustomerID, display = n.CompanyName }).ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        // [DonutOutputCache(CacheProfile = "YearNoParam")]
+        public JsonResult GetShipVia(int id) {
+            var data = db.Shippers.Where(n=> n.ShipperID == id).Select(n => new { id = n.ShipperID, display = n.CompanyName }).ToList();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion
  
     }
 	
