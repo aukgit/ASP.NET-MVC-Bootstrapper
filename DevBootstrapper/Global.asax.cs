68ac7da8a19ae0eb05d38fa50209f74a4bfd8618
Alim Ul Karim <devorg.bd@gmail.com>
2015-06-16 23:35:38 +0600

    scheduler added

--- DevBootstrapper/Global.asax.cs
+++ DevBootstrapper/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using WereViewApp.Scheduler;
+using FluentScheduler;
 
 namespace DevBootstrapper {
     public class MvcApplication : HttpApplication {
@@ -14,17 +16,19 @@ namespace DevBootstrapper {
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
             MvcHandler.DisableMvcResponseHeader = true;
+            TaskManager.Initialize(registry: new SchedulerRunner());
         }
 
         public override string GetVaryByCustomString(HttpContext context, string arg) {
             if (arg != null && arg.Equals("byuser", StringComparison.OrdinalIgnoreCase) ||
                 arg.Equals("user", StringComparison.OrdinalIgnoreCase)) {
-                var cookie = context.Request.Cookies["ASP.NET_SessionID"];
-                if (cookie != null) {
-                    return cookie.Value;
+                //HttpCookie cookie = context.Request.Cookies["ASP.NET_SessionID"];
+                //if (cookie != null) {
+                //    return cookie.Value.ToString();
                     //} else {
                     //    return "const-none";
-                }
+                //}
+                return User.Identity.Name;
             }
             return base.GetVaryByCustomString(context, arg);
         }
